c001dbcdf589e5133d3f8efdd701a1ea034f5ec7
Eben English <eben.english@gmail.com>
2025-03-07 17:15:14 -0500

    updates to config settings [skip ci]

--- app/models/commonwealth_search_builder.rb
+++ app/models/commonwealth_search_builder.rb
@@ -5,14 +5,23 @@ class CommonwealthSearchBuilder < Blacklight::SearchBuilder
   # include BlacklightAdvancedSearch::AdvancedSearchBuilder
   # include BlacklightMaps::MapsSearchBuilderBehavior
   include BlacklightRangeLimit::RangeLimitBuilder
-  include CommonwealthVlrEngine::CommonwealthSearchBuilderBehavior
+  # include CommonwealthVlrEngine::CommonwealthSearchBuilderBehavior
+  include CommonwealthVlrEngine::SearchBuilderBehavior
 
+  # NEW
   self.default_processor_chain += [
+    :institution_limit, :exclude_institutions,
     :site_filter, :exclude_unwanted_models, :exclude_unpublished_items #,
     # :add_advanced_search_to_solr # :add_advanced_parse_q_to_solr,
   ]
 
-  unless I18n.t('blacklight.home.browse.institutions.enabled')
-    self.default_processor_chain += [:institution_limit, :exclude_institutions]
-  end
+  # OLD
+  #self.default_processor_chain += [
+  #  :site_filter, :exclude_unwanted_models, :exclude_unpublished_items #,
+  #  # :add_advanced_search_to_solr # :add_advanced_parse_q_to_solr,
+  #]
+
+  #unless I18n.t('blacklight.home.browse.institutions.enabled')
+  #  self.default_processor_chain += [:institution_limit, :exclude_institutions]
+  #end
 end
